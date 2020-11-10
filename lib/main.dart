2ed11367de9bc632749d4b98641f71bcb2496e9e
alejandroem <alejandro@bixlabs.com>
2020-11-10 12:50:24 +0100

    Add state to the app Add Scaffold to the app Add events to buttons Add private properties to app

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,54 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
+main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
 }
 
-class MyApp extends StatelessWidget {
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
+
+  void _answerQuestion() {
+    setState(() {
+      _questionIndex = _questionIndex + 1;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp( home: Text('Hello!'),);
+    var questions = [
+      'Whats your favorite color?',
+      'Whats your favorite animal?',
+    ];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My first app'),
+        ),
+        body: Column(
+          children: [
+            Text(
+              questions[_questionIndex],
+            ),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: _answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer 2'),
+              onPressed: _answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer 2'),
+              onPressed: _answerQuestion,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
