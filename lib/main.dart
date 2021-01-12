9f4f6fa8d56c806d72d4e041ea3ebb3dc1decd28
alejandroem <alejandro@bixlabs.com>
2021-01-12 14:09:52 +0100

    Added reset handler

--- lib/main.dart
+++ lib/main.dart
@@ -93,6 +93,13 @@ class _MyAppState extends State<MyApp> {
     }
   }
 
+  void _resetQuiz() {
+    setState(() {
+      _questionIndex = 0;
+      _totalScore = 0;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -106,7 +113,7 @@ class _MyAppState extends State<MyApp> {
                 questionIndex: _questionIndex,
                 questions: _questions,
               )
-            : Result(_totalScore),
+            : Result(_totalScore, _resetQuiz),
       ),
     );
   }
