b37555ff58c38fe537da738fb63c18d999ae97de
alejandroem <alejandro@bixlabs.com>
2021-01-21 12:48:03 +0100

    String interploation

--- personalExpenses/lib/main.dart
+++ personalExpenses/lib/main.dart
@@ -49,18 +49,33 @@ class MyApp extends StatelessWidget {
                             EdgeInsets.symmetric(vertical: 10, horizontal: 15),
                         decoration: BoxDecoration(
                             border: Border.all(
-                          color: Colors.black,
+                          color: Colors.purple,
                           width: 2,
                         )),
                         child: Text(
-                          tx.amount.toString(),
-                          style: TextStyle(fontWeight: FontWeight.bold),
+                          '\$${tx.amount}',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                              color: Colors.purple),
                         ),
                       ),
                       Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          Text(tx.title),
-                          Text(tx.date.toString()),
+                          Text(
+                            tx.title,
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            tx.date.toString(),
+                            style: TextStyle(
+                              color: Colors.grey,
+                            ),
+                          ),
                         ],
                       )
                     ],
